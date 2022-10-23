3d568e5805dd9262dd87b7f9d46aa0a2fb4894e6
thgribeiro <thiagordlr@gmail.com>
2022-10-23 00:58:14 -0300

    remoção de chrome_autoinstaller

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-import chromedriver_autoinstaller
 from time import sleep
 import telepot
 import os
